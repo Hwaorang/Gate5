13e1baff31c7da4a865cd6faa29cb924cd937d37
Mondante <133192986+Mondante@users.noreply.github.com>
2026-08-31 18:20:58 +0900

    0831_00

---
+++ Assets/Jin/Scripts/MonIdle_State.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MonIdle_State : MonState<Mon_Ctrl>
+{
+    public void Enter()
+    {
+
+    }
+
+    public void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Jin/Scripts/MonSpawn_Mgr.cs
@@ -0,0 +1,76 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonSpawn_Mgr : MonoBehaviour
+{
+    public static MonSpawn_Mgr instance;
+
+    [SerializeField] List<GameObject> objList = new List<GameObject>();
+
+    Dictionary<string, Queue<GameObject>> pools = new Dictionary<string, Queue<GameObject>>();
+
+    int poolSize = 10;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        foreach (GameObject obj in objList)
+        {
+            pools[obj.name] = new Queue<GameObject>();
+
+            GameObject parentPool = new GameObject($"{obj.name}_Pool");
+            parentPool.transform.SetParent(this.transform);
+
+            for (int i = 0; i < poolSize; i++)
+            {
+                GameObject go = Instantiate(obj, parentPool.transform);
+                go.SetActive(false);
+                pools[obj.name].Enqueue(go);
+            }
+        }
+    }
+
+    public GameObject GetObject(string name)
+    {
+        if (!pools.ContainsKey(name))
+        {
+            Debug.Log("null");
+            return null;
+        }
+
+        if (pools[name].Count > 0)
+        {
+            Debug.Log($"name : {name}");
+            GameObject go = pools[name].Dequeue();
+            go.SetActive(true);
+            return go;
+        }
+        else
+        {
+            GameObject go = Instantiate(objList.Find(obj => obj.name == name));
+            return go;
+        }
+    }
+
+    public void ReturnObject(string name, GameObject go)
+    {
+        Debug.Log("Return");
+        if (!pools.ContainsKey(name))
+        {
+            Destroy(go);
+            return;
+        }
+        go.SetActive(false);
+        pools[name].Enqueue(go);
+    }
+}
