f260beb46b859e392697400783c1ab0aed05ab80
Mondante <133192986+Mondante@users.noreply.github.com>
2026-09-04 13:23:12 +0900

    Revert "0903_03"
    This reverts commit 0c641acc23ba149611066979ab6a9b0f360dd29e.

--- Assets/Jin/Scripts/Mon_Ctrl.cs
+++ Assets/Jin/Scripts/Mon_Ctrl.cs
@@ -90,7 +90,6 @@ public class Mon_Ctrl : MonoBehaviour
 
         if(hp < 0)
         {
-            Debug.Log(name + "mon dead");
             MonSpawn_Mgr.instance.ReturnObject(objname, this.gameObject);
         }
     }
